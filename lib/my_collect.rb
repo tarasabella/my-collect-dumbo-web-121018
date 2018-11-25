da129e6e43e1b8f4f36120604666d48fb02c2513
Tara Sabella <tarasabella@gmail.com>
2018-11-25 01:58:21 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -4,8 +4,6 @@ new_collection = []
 while i < collection.length 
 new_collection << yield(collectio[i])
   i += 1 
-my_collect(students) do |name|
-  name.split(" ").first 
 end 
 my_collect(languages) do |lang|
   lang.upcase
