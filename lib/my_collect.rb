c7349c247012c7e99d5993fc9faed4e7c17d55a1
Tara Sabella <tarasabella@gmail.com>
2018-11-25 01:43:44 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,16 @@
+def my_collect(collection)
+collection = []
+yield(collection)
+  counter += 1 
+my_collect(collection) do |name|
+  name.split(" ").first 
+end 
+my_collect(collection) do |lang|
+  lang.upcase
+end
+end 
 
 
+  
+
+  
